0beeb6ca44c150549beabfe6a80374e9e8be5d00
Ayonathan <ayonathan0@gmai.com>
2022-11-15 21:35:25 +0300

    ughmfmn

--- src/components/log.jsx
+++ src/components/log.jsx
@@ -1,5 +1,5 @@
-import { Layout, Select, Typography, Col, Row, Switch, Form, Input, Button,Alert  } from 'antd';
-import React, {useState, useEffect} from 'react';
+import { Layout, Select, Typography, Col, Row, Switch, Form, Input, Button  } from 'antd';
+import React, {useState} from 'react';
 
 
 const { Title, Text } = Typography;
@@ -33,31 +33,52 @@ const dep_2 =  [ {value: 'Thembani Tywakadi', label: 'Thembani Tywakadi',},
 
 
   const dep_3 =  [ { value: 'Aleksandra Antczak', label: 'Aleksandra Antczak', },
-  { value: 'Ice Rink Supervisor', label: 'Ice Rink Supervisor', },
-  { value: 'Ice Marshall', label: 'Ice Marshall', },
-  { value: 'Skate Exchange', label: 'Skate Exchange', },
-  { value: 'Slide Marshall', label: 'Slide Marshall', },
+  { value: 'Izabella Magdowska', label: 'Izabella Magdowska', },
+  { value: 'Konrad Kita', label: 'Konrad Kita', },
+  { value: 'Ethelmae Jewel Suguitan', label: 'Ethelmae Jewel Suguitan', },
+  { value: 'Gabija Meilunaite', label: 'Gabija Meilunaite', },
+  { value: 'Konrad Kita', label: 'Konrad Kita', },
+  { value: 'Ethelmae Jewel Suguitan', label: 'Ethelmae Jewel Suguitan', },
+  { value: 'Abdulrahman Mohhamed Ahmed Albahri', label: 'Abdulrahman Mohhamed Ahmed Albahri', },
+  { value: 'Jose Fernando', label: 'Jose Fernando', },
+  { value: 'Kamil Kabaszyn', label: 'Kamil Kabaszyn', },
+  { value: 'Saud Aljumairi', label: 'Saud Aljumairi', },
+  { value: 'Brian Njoroge Wanjiku', label: 'Brian Njoroge Wanjiku', },
 ]
 
-const dep_4 =  [ {value: 'Duty Manager', label: 'Duty Manager',},
-                { value: 'Ice Rink Supervisor', label: 'Ice Rink Supervisor', },
-                { value: 'Ice Marshall', label: 'Ice Marshall', },
-                { value: 'Skate Exchange', label: 'Skate Exchange', },
-                { value: 'Slide Marshall', label: 'Slide Marshall', },
+const dep_4 =  [ {value: 'Mohammad Waqas', label: 'Mohammad Waqas',},
+                { value: 'Azreena Shaheed', label: 'Azreena Shaheed', },
+                { value: 'Cecil Varghese', label: 'Cecil Varghese', },
+                { value: 'Konrad Kita', label: 'Skate Exchange', },
+                { value: 'Raees Sulaiman', label: 'Raees Sulaiman', },
+                {value: 'Maryam Salim Jalil', label: 'Maryam Salim Jalil',},
+                { value: 'Fariha Anees', label: 'Fariha Anees', },
+                { value: 'Anne Perera', label: 'Anne Perera', },
+                { value: 'Mohamed Ben Zaid', label: 'Mohamed Ben Zaid', },
+                { value: 'Lois  Acquah', label: 'Lois  Acquah', },
+                { value: 'Arjun s Mohan ', label: 'Arjun s Mohan ', },
+                { value: 'Rajesh Greeson', label: 'Rajesh Greeson', },
+                { value: 'Sen Mathew', label: 'Sen Mathew', },
+                { value: 'Ashik Kurin', label: 'Ashik Kurin', },
+                { value: 'Jason John', label: 'Jason John', },
   ]
 
 
 
-const dep_5 =  [ {value: 'Duty Manager', label: 'Duty Manager',},
-  { value: 'Ice Rink Supervisor', label: 'Ice Rink Supervisor', },
-  { value: 'Ice Marshall', label: 'Ice Marshall', },
-  { value: 'Skate Exchange', label: 'Skate Exchange', },
-  { value: 'Slide Marshall', label: 'Slide Marshall', },
+const dep_5 =  [ {value: 'Jewel Lisa Rose Dsouza', label: 'Jewel Lisa Rose Dsouza',},
+  { value: 'Mishaal Siraj', label: 'Mishaal Siraj', },
+  { value: 'Janindee Onethra Dissanayake', label: 'Janindee Onethra Dissanayake', },
+  { value: 'Arman Miha', label: 'Arman Miha', },
+  { value: 'Abdul Hakim', label: 'Abdul Hakim', },
+  { value: 'Muddasser', label: 'Muddasser', },
+  { value: 'Cedric V. Daysa', label: 'Cedric V. Daysa', },
+  { value: 'Ruth Ann Wanjiku', label: 'Ruth Ann Wanjiku', },
+  { value: 'Kristina Atienza', label: 'Kristina Atienza', },
 ]
 
 const App = () => {
 
-    const [tdy, setTdy] = useState(today)
+    // const [tdy, setTdy] = useState(today)
     const [select_status, setSelect] = useState(true)
     const [other_status, setOthers] = useState(true)
     const [name_list, setName] = useState([])
@@ -74,15 +95,15 @@ const App = () => {
       }
       else if (value === 'Ice Marshall'){
         setSelect(false)
-        setName(dep_1)
+        setName(dep_3)
       }
       else if (value === 'Skate Exchange'){
         setSelect(false)
-        setName(dep_1)
+        setName(dep_4)
       }
       else if (value === 'Slide Marshall'){
         setSelect(false)
-        setName(dep_1)
+        setName(dep_5)
       }
     }
 
@@ -106,7 +127,7 @@ const App = () => {
         <Title style={{color: "white", fontSize:28}}>
             Attendance Log
         </Title>
-        <Text style={{color: "white"}}>{tdy}</Text>
+        <Text style={{color: "white"}}>{today}</Text>
      </Typography>
       </Header>
       <Content>
@@ -115,7 +136,8 @@ const App = () => {
             <Col><Select defaultValue="- Name -"  onChange={handleChange} style={{ width: 180, margin: 6, height: 45}}  disabled={select_status} options={name_list} /></Col> 
             
      </Row>
-     <Row style={{margin: 6}} type="flex" justify="center"> 
+     <Row style={{margin: 6}} type="flex" justify="center">
+      <Col>
         <Form   wrapperCol={{ span: 14,  }} layout="horizontal" >
         <Form.Item label="Present" >
         <Switch disabled ={other_status} />
@@ -127,6 +149,7 @@ const App = () => {
           <Button disabled ={other_status} onClick={handleSubmit}>Submit</Button>
         </Form.Item>
         </Form> 
+        </Col> 
     </Row>
       </Content>
       {/* <Footer>Footer</Footer> */}
